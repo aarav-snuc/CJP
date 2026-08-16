1a9440a08c0351c42f630d45844199b0c97485e1
Student B <mail@aaravs.net>
2026-08-16 21:40:02 +0530

    Accidentally deleted login.py - C

--- src/login.py
+++
@@ -1,6 +0,0 @@
-def login(username, password):
-    if username == "admin" and password == "1234":
-        print("Login successful")
-        return True
-    print("Login failed")
-    return False
